ad8751b968b82cf663515b1c04d4e578a60efce7
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2023-03-08 21:49:48 -0500

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,205 +1,148 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@Aayush-Tadhani246 
+procodingclass
+/
+PRO-C144-Project_Solution
+Public
+Fork your own copy of procodingclass/PRO-C144-Project_Solution
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+You’re making changes in a project you don’t have write access to. We’ve created a fork of this project for you to commit your proposed changes to. Submitting a change will write it to a new branch in your fork, so you can send a pull request.
+PRO-C144-Project_Solution
+/
+screens
+/
+Popular.js
+in
+procodingclass:master
+ 
+
+Spaces
+
+2
+
+No wrap
+1
 import React, { Component } from "react";
+2
 import {
+3
   View,
-  Text,
+4
   StyleSheet,
-  Image,
-  TouchableOpacity,
+5
+  FlatList,
+6
+  Text,
+7
   ImageBackground
+8
 } from "react-native";
-import { Icon } from "react-native-elements";
-import { RFValue } from "react-native-responsive-fontsize";
+9
 import axios from "axios";
-import WebView from "react-native-webview";
-
-export default class HomeScreen extends Component {
-  constructor() {
-    super();
+10
+import { RFValue } from "react-native-responsive-fontsize";
+11
+import Star from "react-native-star-view";
+12
+​
+13
+export default class PopularScreen extends Component {
+14
+  constructor(props) {
+15
+    super(props);
+16
     this.state = {
-      articleDetails: {},
-      ngrok_url: "",
+17
+      data: [],
+18
+      ngrok_url:""
+19
     };
+20
   }
-
+21
+​
+22
   componentDidMount() {
-    this.getArticle();
+23
+    this.getData();
+24
   }
-
-  getArticle = () => {
-    const url = this.state.ngrok_url + "/get-article";
+25
+​
+26
+  getData = () => {
+27
+    const url = this.state.ngrok_url+"/popular-articles";
+28
     axios
+29
       .get(url)
-      .then((response) => {
-        let details = response.data.data;
-        this.setState({ articleDetails: details });
+30
+      .then(async (response) => {
+31
+        this.setState({ data: response.data.data });
+32
       })
+33
       .catch((error) => {
+34
         console.log(error.message);
+35
       });
+36
   };
-
-  likedArticle = () => {
-    const url = this.state.ngrok_url + "/liked-article";
-    axios
-      .get(url)
-      .then((response) => {
-        this.getArticle();
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
-
-  unlikedArticle = () => {
-    const url = this.state.ngrok_url + "/unliked-article";
-    axios
-      .get(url)
-      .then((response) => {
-        this.getArticle();
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
-
-  render() {
-    const { articleDetails } = this.state;
-    const { url } = articleDetails;
-    if (url) {
+37
+​
+38
+  keyExtractor = (item, index) => index.toString();
+39
+​
+40
+  renderItems = ({ item, index }) => {
+41
       return (
-        <View style={styles.container}>
-          <ImageBackground
-            source={require("../assets/bg.png")}
-            style={{ flex: 1 }}
-          >
-            <View style={styles.headerContainer}>
-              <Text style={styles.headerTitle}>Articles to Read</Text>
-              <Icon
-                name="chevron-right"
-                type="feather"
-                color={"white"}
-                size={RFValue(30)}
-                containerStyle={{ position: "absolute", right: RFValue(5) }}
-                onPress={() => {
-                  this.props.navigation.navigate("Articles");
-                }}
-              ></Icon>
-            </View>
-
-            <View style={styles.subContainer}>
-              <WebView source={{ uri: url }} />
-
-              <View style={styles.iconButtonContainer}>
-                <TouchableOpacity onPress={this.likedArticle}>
-                  <Image
-                    style={styles.iconImage}
-                    source={require("../assets/like.png")}
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={this.unlikedArticle}>
-                  <Image
-                    style={styles.iconImage}
-                    source={require("../assets/dislike.png")}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
-          </ImageBackground>
+42
+        <View style={styles.cardContainer}>
+43
+          <Text style={styles.title}>Article name: {"\n\n"+item.title}</Text>
+44
+          <Star score={item.total_events} style={styles.starStyle} />
+45
         </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <ImageBackground
-            source={require("../assets/bg.png")}
-            style={{ flex: 1 }}
-          >
-            <View style={styles.headerContainer}>
-              <Text style={styles.headerTitle}>Articles to Read</Text>
-            </View>
-            <Text style={styles.headerTitle}>Loading...</Text>
-          </ImageBackground>
-        </View>
-      );
-    }
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  headerContainer: {
-    flex: 0.07,
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    backgroundColor: "#3D550C",
-  },
-  headerTitle: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: RFValue(18),
-    fontFamily: "monospace",
-    textAlign: "center",
-    flex: 1,
-  },
-  subContainer: {
-    flex: 0.9,
-  },
-  posterContainer: {
-    flex: 0.6,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  posterImage: {
-    width: "85%",
-    height: "95%",
-    resizeMode: "stretch",
-    borderRadius: RFValue(10),
-    marginHorizontal: RFValue(5),
-  },
-  detailsContainer: {
-    width: "80%",
-    alignSelf: "center",
-    flex: 0.2,
-    backgroundColor: "#3c8ed9",
-    borderRadius: RFValue(10),
-    marginHorizontal: RFValue(10),
-    padding: RFValue(10),
-  },
-  title: {
-    fontSize: RFValue(15),
-    fontWeight: "bold",
-    color: "white",
-    fontFamily: "monospace",
-    marginVertical: RFValue(5),
-  },
-  subtitle: {
-    fontSize: RFValue(10),
-    fontWeight: "bold",
-    color: "white",
-    fontFamily: "monospace",
-    marginVertical: RFValue(5),
-  },
-  ratingContainer: {
-    flex: 0.1,
-  },
-  overview: {
-    fontSize: RFValue(13),
-    color: "white",
-    fontFamily: "monospace",
-    marginVertical: RFValue(5),
-  },
-  iconButtonContainer: {
-    flex: 0.1,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-  },
-  iconImage: {
-    width: RFValue(50),
-    height: RFValue(50),
-  },
-});
+@Aayush-Tadhani246
+Propose changes
+Commit summary
+Create Popular.js
+Optional extended description
+Add an optional extended description…
+  
+ Waiting for your fork…
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
